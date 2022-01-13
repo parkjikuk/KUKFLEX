0d82b8a4bae5d509298ec27586e460676ef3524e
parkjikuk <paxkk@naver.com>
2022-01-14 03:21:28 +0900

    ♻️ Refactor MovieDetail.js

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,43 +1,61 @@
 import PropTypes from "prop-types";
-import {Link} from "react-router-dom";
+import styles from "./MovieDetail.module.css";
 
-function Movie({id, coverImg, rating, runtime, description_full, title, summary, genres}){
-    return(
-    <div>
-        <img src={coverImg} alt={title}/>
-        <h2>
-            <Link to={`/movie/${id}`}>{title}</Link>
-        </h2>
-        <div>
-          <span>{rating ? `rating: ${rating} / 10` : null}</span>
-          <p>{runtime ? `runtime: ${runtime} (min)` : null}</p>
+
+function MovieDetail({ background_image_original, coverImg, rating, runtime, description_full, title, genres }) {
+  return (
+    <div className={styles.movie}>
+      {/* Background Img */}
+      <div className={styles.background}>
+        <img className={styles.Detail_bg} src={background_image_original} />
+      </div>
+      {/* ShortView (Img, Title, rating, runtime...) */}
+      <div className={styles.show}>
+        <div className={styles.shortView}>
+          {/* Img */}
+          <div className={styles.shortView_Img}>
+            <img src={coverImg} alt={title} />
+          </div>
+          {/* title, rating, runtime, genre */}
+          <div className={styles.shortView_letters}>
+            <h3>
+              {title}
+            </h3>
+            <p>{rating ? `rating: ${rating} / 10` : null}</p>
+            <p>{runtime ? `runtime: ${runtime} (min)` : null}</p>
+            {
+              genres ?
+                // genre is the 'array'
+                <div>
+                  <b>{'genres'}</b>
+                  <ul>{genres.map(g => <li key={g}>{g}</li>)}</ul>
+                </div>
+                : null
+            }
+          </div>
         </div>
-        {genres ? 
-           <div>
-             <h3>genres</h3>
-             <ul>{genres.map(g => <li key={g}>{g}</li>)}</ul>
-           </div> 
-            : null  
-      }
 
-       {/* 영화 내용 */}
+        {/* Description */}
         {
           description_full ?
-            <div>
+            <div className={styles.descript}>
               <p>{description_full}</p>
             </div>
             : null
         }
+      </div>
     </div>
-    )
+  )
 }
 
-Movie.propTypes = {
-    id: PropTypes.number.isRequired,
-    coverImg: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-  };
+MovieDetail.prototypes = {
+  background_image_original: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+  runtime: PropTypes.number,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description_full: PropTypes.string,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
+}
 
-export default Movie;
+export default MovieDetail;
