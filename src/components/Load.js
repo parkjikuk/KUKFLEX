32369926b580fe8dc5ac3e8ae980b777e4080667
parkjikuk <paxkk@naver.com>
2022-01-18 04:00:06 +0900

    Load component and styles

--- src/components/Load.js
+++ src/components/Load.js
@@ -1,6 +1,8 @@
+import styles from './Load.module.css';
+
 function Load() {
     return (
-        <div>
+        <div className={styles.load}>
             <h1>Loading ...</h1>
         </div>
     )
