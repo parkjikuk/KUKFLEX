dd5c5488bbb0192cfe08edea6a1595c981b932d2
parkjikuk <paxkk@naver.com>
2022-01-18 04:01:30 +0900

    Home component styles use fortawesome

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,19 +2,25 @@ import {Link} from "react-router-dom";
 import styles from "./Home.module.css";
 import Slide from "../components/Slide";
 import navList from "../atom/NavList";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faVideo} from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
   return (
       <div className={styles.container}>
         {navList.map(slide => {
           return (
-            <div className={styles.slide__box}>
-                <h3 className={styles.title}>
+            <div className={styles.slideBox}>
+                <div className={styles.slideTitle}>
                   <Link to={`/page/${slide.path}/1`}>
-                    <i class="fas fa-external-link-alt"></i>
-                    <span>{slide.title} Movie</span>
+                    <div className={styles.titleIcon}>
+                      <FontAwesomeIcon  icon={faVideo} />
+                    </div>
+                    <div>
+                      <span>{slide.title}</span>
+                    </div>
                   </Link>
-                  </h3>
+                </div>
                 <Slide ytsApi={`https://yts.mx/api/v2/list_movies.json?limit=10&${slide.path}&sort_by=year`} />
             </div>
           )
