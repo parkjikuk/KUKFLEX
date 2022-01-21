07f66dc757cf761896aa30c7620a7d694bfd08d0
parkjikuk <paxkk@naver.com>
2022-01-21 23:51:16 +0900

    MovieGroup component and styles

--- src/components/MovieGroup.js
+++ src/components/MovieGroup.js
@@ -3,16 +3,15 @@ import styles from "./MovieGroup.module.css";
 
 function MovieGroup({id, coverImg, genres, summary, title,  year}) {
     return (
-        <div className={styles.movie} >        
-           <img src={coverImg} className={styles.movie_img} alt={title}/>
-           <div>
-              <h2 className={styles.movie_title}>
-                  <Link to={`/movie/${id}`}>
+        <Link to={`/movie/${id}`}>
+          <div className={styles.movie} >        
+            <img src={coverImg} className={styles.movie_img} alt={title}/>
+            <div>
+              <h2 className={styles.movie_title}>            
                       {(title.length > 35)
                       ? `${title.slice(0,35)}` :
                       title
-                      }
-                  </Link>
+                      }   
               </h2>
               <h3 className={styles.movie_year}>{year}</h3>
               <p className={styles.movie_summary}>{summary ? (summary.length > 180 ? `${summary.slice(0, 180)}...` : summary) : null}</p>
@@ -22,7 +21,8 @@ function MovieGroup({id, coverImg, genres, summary, title,  year}) {
                   )}
               </ul>
              </div>
-        </div>
+          </div>
+         </Link>
       
     )
 }
