a9a7835376b791e9ea8a6594cd4cbf41b64181a0
parkjikuk <paxkk@naver.com>
2022-01-26 02:25:00 +0900

    Implement Home page loading

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,28 +4,44 @@ import Slide from "../components/Slide";
 import navList from "../atom/NavList";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faVideo} from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState, useCallback} from 'react';
+import Load from '../components/Load';
 
 function Home() {
+  const [loading, setLoading] = useState(true);
+
+  const getMovies = useCallback(async () => {
+     await (
+      await fetch(`https://yts.mx/api/v2/list_movies.json?limit=10`)
+    ).json();
+
+    setLoading(false);
+  }, [])
+
+  useEffect(() => {
+    setLoading(true);
+    getMovies();
+  }, [])
+
   return (
       <div className={styles.container}>
-        {navList.map(slide => {
+        {loading ? <Load /> : navList.map(slide => {
           return (
             <div className={styles.slideBox} key={slide.title}>
-                <div className={styles.slideTitle}>
-                  <Link to={`/page/${slide.path}/1`}>
-                    <div className={styles.titleIcon}>
-                      <FontAwesomeIcon  icon={faVideo} />
-                    </div>
-                    <div>
-                      <span>{slide.title}</span>
-                    </div>
-                  </Link>
+            <div className={styles.slideTitle}>
+              <Link to={`/page/${slide.path}/1`}>
+                <div className={styles.titleIcon}>
+                  <FontAwesomeIcon  icon={faVideo} />
                 </div>
-                <Slide  ytsApi={`https://yts.mx/api/v2/list_movies.json?limit=10&${slide.path}&sort_by=year`} />
+                <div>
+                  <span>{slide.title}</span>
+                </div>
+              </Link>
             </div>
+            <Slide  ytsApi={`https://yts.mx/api/v2/list_movies.json?limit=10&${slide.path}&sort_by=year`} />
+        </div>
           )
-        })}
-        
+        })}       
       </div>
   )
 }
