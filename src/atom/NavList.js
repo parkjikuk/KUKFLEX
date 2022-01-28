11e41dfc25bbc1d2005c6f5ec244c6ac662c1ab3
parkjikuk <paxkk@naver.com>
2022-01-29 02:05:37 +0900

    edit text

--- src/atom/NavList.js
+++ src/atom/NavList.js
@@ -1,15 +1,20 @@
-const navList = [{
+const navList = [
+  {
     title : "High Rating",
-    path: "minimum_rating=7"
-  }, {
+    path: "minimum_rating=8"
+  },
+   {
     title : "Romance",
     path: "genre=romance"
-  }, {
+  },
+   {
     title : "Thriller",
     path: "genre=thriller"
-  }, {
+  },
+   {
     title : "Animation",
     path: "genre=animation"
-  }]
+  }
+]
 
 export default navList;
