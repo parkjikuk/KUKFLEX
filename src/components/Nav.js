4b0ffd98322c1d6d2a4ebb976723d97f8a331c25
parkjikuk <paxkk@naver.com>
2022-02-03 20:10:54 +0900

    Add responsive tablet,mobile menu toggle

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,12 +5,14 @@ import navList from "../atom/NavList";
 import styles from "./Nav.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
 
 
 function Nav(){
     // const [search, setSearch] = useState(null);
     const pageReLoading = useSetRecoilState(listPageReLoading);
     const [focusPath, setFocusPath] = useRecoilState(focusNav);
+    const [menuOpen, setMenuOpen] = useState(false);
 
 
     // const onClick = (event) => {
@@ -19,7 +21,13 @@ function Nav(){
 
     const optionOnClick = () => {
         pageReLoading(true);
+        setMenuOpen(false);
         console.log(focusPath);
+    }
+
+    const menuClick = () => {
+        setMenuOpen (menuOpen => !menuOpen);
+        console.log(menuOpen);
     }
     
     return(
@@ -29,21 +37,41 @@ function Nav(){
             </div>
 
             <div className={styles.title}>
+              <ul className={styles.movie_title}>
                  {navList.map(({title, path}) => {
                    return (
-                        <div className={styles.movie_title} key={title}>
+                        <li key={title}>
                             <Link to={`/page/${path}/1`}
                              onClick={focusPath !== path ? optionOnClick : null}
                              style={focusPath !== path ? null : {
                              color: "#00FFFF",
                                }} 
                         >{title}</Link>
-                        </div>                                   
+                        </li>                                   
                    )
                  })}
-                 <button className={styles.menu}>
+              </ul>
+
+
+               <div className={styles.menu}>
+                 <button className={styles.menuIcon} onClick={()=>menuClick()}>
                     <FontAwesomeIcon icon={faBars} size="2x"></FontAwesomeIcon>
                  </button>
+                  <ul className={menuOpen ? `${styles.openMenu}` : `${styles.closeMenu}`}>
+                   {navList.map(({title, path}) => {
+                     return (
+                          <li className={styles.menuItem} key={title}>
+                              <Link to={`/page/${path}/1`}
+                               onClick={focusPath !== path ? optionOnClick : null}                   
+                               style={focusPath !== path ? null : {
+                               color: "#00FFFF",
+                                 }} 
+                          >{title}</Link>
+                          </li>                                   
+                     )
+                   })}
+                   </ul>           
+               </div>
             </div>
         </div>
     )
